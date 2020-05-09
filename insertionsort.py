13aa1b6ad9b619d3642aa56dcb3ab872417b7470
Summer Gautier <summergautier@gmail.com>
2020-05-08 20:45:10 -0700

    finished iterative sort method for insertion sort

--- insertionsort.py
+++ insertionsort.py
@@ -9,19 +9,35 @@ def recursiveSort(listOfItems: list, startIndex: int, endIndex: int) -> list:
     if(startIndex == endIndex):
         return [listOfItems[startIndex]]
     else:
-        subList = recursiveSort(listOfItems, startIndex-1, endIndex)
+        subList: list = recursiveSort(listOfItems, startIndex-1, endIndex)
+        largest: bool = True
         for index, item in enumerate(subList):
             if(item > listOfItems[startIndex]):
                 subList.insert(index, listOfItems[startIndex])
+                largest = False
                 break
+        if(largest):
+            subList.append(listOfItems[startIndex])
+        
         return subList
+
+#ITERATIVE
+def iterativeSort(listOfItems: list) -> list:
+    for index, item in enumerate(listOfItems):
+        for subListIndex in range(0, index):
+            if(listOfItems[subListIndex] > item):
+                del listOfItems[index]
+                listOfItems.insert(subListIndex, item)
+                break
+
+    return listOfItems
+
 #TEST SORT METHODS
 class SortTest(unittest.TestCase):
     def test(self):
         sampleList = [18,5,6,1,7,3,23,8,10]
-        #self.assertEqual(iterativeSort(sampleList), [1,3,5,6,7,8,10,18,23]),
-        self.assertEqual(recursiveSort(sampleList, 0, len(sampleList)-1), [1,3,5,6,7,8,18,23])
+        self.assertEqual(iterativeSort(sampleList), [1,3,5,6,7,8,10,18,23]),
+        self.assertEqual(recursiveSort(sampleList, len(sampleList)-1, 0), [1,3,5,6,7,8,10,18,23])
 
 if __name__ == '__main__':
-    sampleList = [18,5,6,1,7,3,23,8,10]
-    print(recursiveSort(sampleList, len(sampleList)-1, 0))
+    print(str(unittest.main()))
