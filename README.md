9e608532bd3b722eb226fcb99456ef5d5b401f96
Summer Gautier <summergautier@gmail.com>
2020-05-12 08:34:48 -0700

    add definition of merge sort

--- README.md
+++ README.md
@@ -7,4 +7,5 @@ Recursive and Iterative implementation of various sorting algorithms
     - With the first two items of the list, sort them in the correct order. Then, insert the third item into the sorted subset of the list. Repeat with the fourth item, fifth item, etc. Keep inserting items into the sorted subset until all items in the list have been sorted.
 + Bubble sort
     - For each item (A) at a position 'n' in the list, compare it with the item (B) in position 'n+1' in the list. If the item A is greater than item B, then swap items A and B. Following this system, after one pass, the largest unsorted item will make it's final position in the unsorted array. Repeat until the entire list is sorted.
-  
++ Merge sort
+    - Divide the the list into halves until you've reached the smallest possible list (a single item). Then, sort the sublists and merge them until you've created a complete sorted list
--- mergesort.py
+++ mergesort.py
@@ -4,14 +4,55 @@
 
 import unittest
 
+#Recursive
+def recursiveSort(listOfItems: list) -> list:
+    if(len(listOfItems) == 1):
+        return listOfItems
 
+    #Take into account odd/even length of lists
+    lenUpperHalf: int = (len(listOfItems)/2) if (len(listOfItems)%2 == 0) else (len(listOfItems)/2)+0.5;
+    lenLowerHalf: int = (len(listOfItems)/2) if (len(listOfItems)%2 == 0) else (len(listOfItems)/2)-0.5;
+
+    #split list into halves
+    list1: list = listOfItems[:int(lenUpperHalf)]
+    list2: list = listOfItems[int(lenLowerHalf):]
+
+    #make recursive call on rest of items
+    list1 = recursiveSort(list1)
+    list2 = recursiveSort(list2)
+    return merge(list1, list2)
+
+#Iterative
+
+
+#Helper Method
+def merge(listA: list, listB: list) -> list:
+    listC: list = []
+    #add the elements of listA and listB in order of whichever is greater
+    for a, b in zip(listA, listB):
+        if(listA[0] > listB[0]):
+            listC.append(listB[0])
+            listB.pop(0)
+        else:
+            listC.append(listA[0])
+            listA.pop(0)
+    return listA
+
+    # if either listA or listB still have elements, add those to the end of listC
+    for a in listA:
+        listC.append(listA[0])
+        listA.pop(0)
+    for b in listB:            
+        listC.append(listB[0])
+        listB.pop(0)
+    return listC
 
 #TEST SORT METHODS
 class SortTest(unittest.TestCase):
     def test(self):
         sampleList: list = [7,5,28,16,9,14,25]
-        self.assertEqual(iterativeSort(sampleList), [5,7,9,14,16,25,28])
-        self.assertEqual(recursiveSort(sampleList, len(sampleList)), [5,7,9,14,16,25,28])
+        #self.assertEqual(iterativeSort(sampleList), [5,7,9,14,16,25,28])
+        self.assertEqual(recursiveSort(sampleList), [5,7,9,14,16,25,28])
 
 if __name__ == '__main__':
     print(str(unittest.main()))
